38aaa789f11ee050f9f78abdd6fb124c1e048d00
yaksa <bemo@live.cn>
2021-06-13 15:05:28 +0800

    Update penzi.py

--- penzi.py
+++ penzi.py
@@ -1,14 +1,24 @@
 #coding=utf-8
 import win32api
+import win32clipboard as w
 import win32con
 import time
 
-print("把要骂的话复制到粘贴板，打开本程序，在10秒内打开聊天窗口")
+print("-----------------------------------------------------------------------")
+print("设置好参数，在10秒内打开聊天窗口")
 print()
-print("                                 -------------------------code by bemo")
+print("                                        --------code by bemo")
+print("-----------------------------------------------------------------------")
+words = input("要骂的话:")
 numb = input("骂人次数:")
-
 time.sleep(8)
+
+def setText(aString):#写入剪切板  
+    w.OpenClipboard()  
+    w.EmptyClipboard()  
+    w.SetClipboardText(aString)  
+    w.CloseClipboard()
+
 
 def kaima():
         win32api.keybd_event(17,0,0,0)  #ctrl
@@ -20,6 +30,7 @@ def kaima():
         time.sleep(0.5)
 
 i = 0
+setText(words)
 while int(i) <= int(numb):
     kaima()
     i =  i + 1
